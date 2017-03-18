567b3ba3893b8947033907d8e5c23579bc9174a7
Realan80 <realan80@gmail.com>
2017-03-18 22:14:16 +0100

    Removed shit

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
-  	t.identity = "flight"
-  	t.externalstorage = true
-  	t.accelerometerjoystick = true
-  	t.window.width = 1280    																		
+    t.identity = "flight"
+    t.externalstorage = true
+    t.accelerometerjoystick = true
+    t.window.width = 1280
     t.window.height = 960
     t.window.fullscreen = false
     t.window.fullscreentype = "desktop"
